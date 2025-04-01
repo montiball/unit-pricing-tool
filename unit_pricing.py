1a30b447b78a5cc9b34a72b13be614b03a5d51ab
montiball <montiball@hotmail.com>
2025-04-01 16:37:09 -0400

    unit_pricing.py

--- unit_pricing.py
+++ unit_pricing.py
@@ -231,7 +231,7 @@ with tab2:
 
         try:
             response = client.chat.completions.create(
-                model="gpt-4",
+                model="gpt-3.5-turbo",
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.7,
                 max_tokens=600
